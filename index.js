e8a2ad97948c7294fc6aca0c9507a63f8049e5a2
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-23 14:16:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+const map = (sourceArray, func) => {
+  const newArray = [];
+  for (const n of sourceArray) {
+    newArray.push(func(n));
+  }
+  return newArray;
+};
+
+const reduce = (sourceArray, func, startingPoint = 0) => {
+  let memo;
+  if (startingPoint) {
+    memo = startingPoint;
+    for (let i=0; i<sourceArray.length; i++) {
+      memo = func(memo, sourceArray[i]);
+    }
+  } else {
+    memo = sourceArray[0];
+    for (let i = 1; i < sourceArray.length; i++) {
+      memo = func(memo, sourceArray[i]);
+    }
+  }
+
+  return memo;
+};
